1e049c8abbe4bbdd976e6d70c61868f3966d33e4
Alex Milanov <alex@i4web.biz>
2016-10-24 18:16:47 +0300

    ui update, about us page

--- src/js/ui/header/index.js
+++ src/js/ui/header/index.js
@@ -5,34 +5,44 @@ const {
 	table, thead, tbody, tr, td, th, form, input, button, label
 } = require('../../util/vdom');
 
+const links = [
+	{page: 'home', href: '#/', title: 'Начало'},
+	{page: 'about', href: '#/about', title: 'За Нас'},
+	{page: 'almanac', href: '#/almanac', title: 'Алманах'},
+	{page: 'research', href: '#/research', title: 'Изследвания'},
+	{page: 'links', href: '#/links', title: 'Връзки'}
+];
+
 module.exports = ({state, actions}) => header([
 	section('.title', [
 		h1('Дружество Родолюбец'),
 		h3('Културно-просветно дружество за връзки с бесарабските и таврийските българи')
 	]),
-	ul('#menu', [
-		li([a({attrs: {href: '#/'}, class: {active: state.route.page === 'home'}}, 'Начало')]),
-		li([a({attrs: {href: '#/about'}, class: {active: state.route.page === 'about'}}, 'За Нас')]),
-		li([a({attrs: {href: '#/almanac'}, class: {active: state.route.page === 'almanac'}}, 'Алманах')]),
-		li([a({attrs: {href: '#/research'}, class: {active: state.route.page === 'research'}}, 'Изследвания')]),
-		li([a({attrs: {href: '#/links'}, class: {active: state.route.page === 'links'}}, 'Връзки')]),
-		li('.right', [
-			(state.signInToggled)
-				? form([
-					label('[for="login-user"]', [i('.fa.fa-user')]),
-					input('#login-user[name="user"][placeholder="Потребителско име"]'),
-					label('[for="login-pass"]', [i('.fa.fa-key')]),
-					input('#login-pass[name="pass"][type="password"][placeholder="Парола"]'),
-					button([
-						i('.fa.fa-sign-in')
-					]),
-					a({on: {click: () => actions.signInToggle()}}, [
-						i('.fa.fa-close')
-					])
+	ul('#menu', links.map(link =>
+		li([a(
+			`[href="${link.href}"]`,
+			{class: {active: link.page === state.route.page}},
+			link.title
+		)])
+	).concat([
+		li('.right', [(state.signInToggled)
+			// login form
+			? form([
+				label('[for="login-user"]', [i('.fa.fa-user')]),
+				input('#login-user[name="user"][placeholder="Потребителско име"]'),
+				label('[for="login-pass"]', [i('.fa.fa-key')]),
+				input('#login-pass[name="pass"][type="password"][placeholder="Парола"]'),
+				button([
+					i('.fa.fa-sign-in')
+				]),
+				a({on: {click: () => actions.signInToggle()}}, [
+					i('.fa.fa-close')
 				])
-				: a({on: {click: () => actions.signInToggle()}}, [
-					i('.fa.fa-pencil-square-o')
-				])
+			])
+			// toggle button
+			: a({on: {click: () => actions.signInToggle()}}, [
+				i('.fa.fa-pencil-square-o')
+			])
 		])
-	])
+	]))
 ]);
