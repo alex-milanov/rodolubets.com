138994f2c7a024bbbd1ecf0f3e594adb5335d327
Alex Milanov <alex@i4web.biz>
2016-10-26 20:15:03 +0300

    potentialy background texture

--- bin/move-assets.js
+++ bin/move-assets.js
@@ -4,13 +4,21 @@ const fse = require('fs-extra');
 const path = require('path');
 
 const paths = {
-	'dist/fonts': 'node_modules/font-awesome/fonts',
-	'dist/fonts': 'assets/fonts'
+	'dist/fonts': ['node_modules/font-awesome/fonts', 'assets/fonts'],
+	'dist/img': 'assets/img'
 };
 
-Object.keys(paths).forEach(
-	p => fse.copySync(
-		path.resolve(__dirname, '..', paths[p]),
-		path.resolve(__dirname, '..', p)
-	)
-);
+Object.keys(paths)
+	.forEach(
+		p => (paths[p] instanceof Array)
+			? paths[p].forEach(v =>
+				fse.copySync(
+					path.resolve(__dirname, '..', v),
+					path.resolve(__dirname, '..', p)
+				)
+			)
+			: fse.copySync(
+				path.resolve(__dirname, '..', paths[p]),
+				path.resolve(__dirname, '..', p)
+			)
+	);
